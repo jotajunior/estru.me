fe9d4283773dccf0e423a5ce2470959a3f0bac58
Jota Junior <jotavrj@gmail.com>
2013-08-05 16:41:12 -0300

    Fixing more merge conflicts

--- src/Estrume/Controller/Link.php
+++ src/Estrume/Controller/Link.php
@@ -6,41 +6,37 @@ use Estrume\Config as Config;
 
 class Link
 {
-	private $model;
+        private $model;
 
-	public function __construct()
-	{
-		$this->model = new Model\Link();
+        public function __construct()
+        {
+                $this->model = new Model\Link();
+                $this->loader = new \Twig_Loader_Filesystem(Config\Path::template);
+                $this->view = new \Twig_Environment($this->loader, array('cache' => Config\Path::cache, 'debug'=>true));
+        }
 
-		$this->loader = new \Twig_Loader_Filesystem(Config\Path::template);
-		$this->view = new \Twig_Environment($this->loader, array('cache' => Config\Path::cache, 'debug'=>true));
-	}
-	
-	public function index()
-	{
-		echo $this->view->render("shortener/index.html", array());
-	}
-	
-	public function shorten($url)
-	{
-		$shortened = $this->model->shorten($url);
-		echo $this->view->render("shortener/result.html", array("shortened" => $shortened));
-	}
-	
-	public function redirect($code)
-	{
-		$url = $this->model->getOriginal($code);
-		if ($url != false)
-			echo $this->view->render("redirect/index.html", array("url" => $url));
-	}
-	
-	public function about()
-	{
-		echo $this->view->render("about/index.html", array());
-	}
-	
-	public function checkForHits($ip)
-	{
-		return $this->model->checkForHits($ip);
-	}
+        public function index()
+        {
+                echo $this->view->render("shortener/index.html", array());
+        }
+
+        public function shorten($url, $ip)
+        {
+                if ($this->model->checkForHits($ip)) {
+                        $shortened = $this->model->shorten($url);
+                        echo $this->view->render("shortener/result.html", array("shortened" => $shortened));
+                }
+        }
+
+        public function redirect($code)
+        {
+                $url = $this->model->getOriginal($code);
+                if ($url != false)
+                        echo $this->view->render("redirect/index.html", array("url" => $url));
+        }
+
+        public function about()
+        {
+                echo $this->view->render("about/index.html", array());
+        }
 }
